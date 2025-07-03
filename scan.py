962d7b88dc937b517c23b096a0ca5dac3c24e45a
zero3119 <coco.adan31@gmail.com>
2025-07-03 00:14:24 -0600

    added the update database function

--- scan.py
+++ scan.py
@@ -1,17 +1,20 @@
-import requests
-from dotenv import load_dotenv
 import os
 import time
-import sqlalchemy as db
+import requests
 import pandas as pd
-from sqlalchemy import Table, Column, String, MetaData, insert
+from dotenv import load_dotenv
+
+import sqlalchemy as db
+from sqlalchemy import Table, Column, String, MetaData, insert, update
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
+
 from google import genai
 from google.genai import types
 
 # Load environment variables for API-KEY
 load_dotenv()
+
 api_key = os.environ.get('API_KEY')
 
 url = "https://www.virustotal.com/api/v3/urls"
@@ -34,16 +37,20 @@ def get_analysis_url():
     # Call post request to obtain analysis ID for the url
     response = requests.post(url, data=payload, headers=headers)
     print(response.status_code)
+
+    # Extract analysis URL from response to get status information
     analysis_url = response.json()["data"]["links"]["self"]
     print(analysis_url)
     return analysis_url
 
+# Function to get analysis statistics using analysis URL
 def get_analysis_stats(url):
     analysis_response = requests.get(url, headers=headers)
     analysis_stats = analysis_response.json()["data"]["attributes"]["stats"]
     print(analysis_stats)
     return analysis_stats
 
+# Function to get a more comprehensive analysis of the information using geminiAI
 def get_summary(stats):
     ai_key = os.environ.get('GENAI_KEY')
     genai.api_key = ai_key
@@ -62,10 +69,12 @@ def get_summary(stats):
     )
     print(response.text)
 
+# Function to set up a conection to SQLite database and fill it with breach information
 def get_db():
     engine = db.create_engine('sqlite:///data_base_name.db')
     metadata = MetaData()
 
+    # Defines the columns of the table
     breaches_table = Table('breaches',metadata,
                          Column('email',String),
                          Column('breach', String),
@@ -74,10 +83,12 @@ def get_db():
                          db.PrimaryKeyConstraint('email', 'breach')
                          )
     
+    # creates table
     metadata.create_all(engine)
     return engine, breaches_table
 
-def get_Pwned(email):
+# Function to check if the email has been breached
+def get_pwned(email):
     url = f"https://haveibeenpwned.com/api/v3/breachedaccount/{email}?truncateResponse=false"
     headers = {
         "HIBP-API-KEY": os.environ.get('HIBP_API_KEY')
@@ -89,6 +100,7 @@ def get_Pwned(email):
     else:
         return analysis_breaches.status_code, []
 
+# Function to get suggestion on how to handle a breach using GgeminiAI 
 def get_suggestions(breach_list):
     ai_key = os.environ.get('GENAI_KEY')
     genai.api_key = ai_key
@@ -107,15 +119,15 @@ def get_suggestions(breach_list):
     )
     print(response.text)
 
-
+#Function that Inserts breach info to the database givin a certain email
 def data_base(email):
     engine, breaches_table = get_db()
     
-    status, breaches = get_Pwned(email)
+    status, breaches = get_pwned(email)
 
     if len(breaches) == 0:
         print("There Have been no breaches in that email")
-        return
+        return []
 
     with engine.connect() as connection:
         for breach in breaches:
@@ -128,7 +140,7 @@ def data_base(email):
 
             try:
                 connection.execute(stmt)
-            except IntegrityError:
+            except IntegrityError: #Ignore duplicate entries
                 pass
         connection.commit()
 
@@ -138,14 +150,38 @@ def data_base(email):
         ), {'email': email}).fetchall()
 
         breach_date_list = [(row[0], row[1]) for row in result]
-        for breach_date in breach_date_list:
-            print(f"Breach: {breach_date[0]}\nDate: {breach_date[1]}\n")
         return breach_date_list
 
-def update_data_base():
-    pass
+# Function to update breach status to fixed in database
+def update_data_base(email, breach_list):
+    engine = db.create_engine('sqlite:///data_base_name.db')
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+
+    breaches_table = metadata.tables['breaches']
+
+    with engine.connect() as connection:
+        for breach_name in breach_list:
+            stmt = (
+                update(breaches_table)
+                .where(
+                    breaches_table.c.email == email,
+                    breaches_table.c.breach == breach_name
+                )
+                .values(fixed='true')
+            )
+
+            result = connection.execute(stmt)
+            connection.commit()
+    
+    # Print cinfirmation of fixed breaches
+    for i, breach_name in enumerate(breach_list):
+        print(f"{i+1}. {breach_name}" )
+
+    print("These breaches were fixed")
 
 
+# Main Program/ Menu System 
 def main():
     print("Welcome to Malwatch where we help you do better in your web safety")
     email = input("\nPlease input your email so we can get started: ")
@@ -165,11 +201,45 @@ def main():
                 change_email = input("Would you like to check a different email?(y/n): ")
                 if change_email=='y' or change_email=='Y':
                     email = input("Enter new email to check: ")
+
             breached_list = data_base(email)
+            for breach_date in breached_list:
+                print(f"Breach: {breach_date[0]}\nDate: {breach_date[1]}\n")
+
             get_suggestions(breached_list)
 
         elif ans == '2':
-            update_data_base()
+            breached_list = data_base(email)
+            if len(breached_list) == 0:
+                print("There are no breaches to update\n")
+                continue
+
+            for ind, breach in enumerate(breached_list):
+                print(f"{ind+1}. {breach[0]}")
+
+            print("Enter the numbers of the breaches fixed seperated by a space "
+            "or type 'all' if you fixed them all: ")
+
+            fixed = input('\nEnter the numbers of the breaches fixed seperated by a space \n'
+            'Type "all" if you fixed them all\n' \
+            'Click "ENTER" to cancel:\n')
+            fixed = fixed.split()
+
+            breached = []
+
+            if len(fixed) == 0:
+                print("No Changes made")
+                continue
+
+            if fixed[0] == "all":
+                for breach in breached_list:
+                    breached.append(breach[0])
+                update_data_base(email, breached)
+            else:
+                for i in fixed:
+                    index = int(i)
+                    breached.append(breached_list[index-1][0])
+                update_data_base(email, breached)
 
         elif ans == '3':
             analysis_url = get_analysis_url()
